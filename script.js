e41ec0d9d5d0044f7e1dcd88c342044a79f7cfdc
Roia Tembo <roia@itmogul.itmogul.co.za>
2024-02-15 00:23:28 +0200

    added loader functionality

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash api
 const count = 10;
 const apiKey = "oZ1RWDX3sQ3_VeS33GhgPWaNpf09xnVrnGGTXaLma64";
 const apiUrl = "https://api.unsplash.com/photos/?client_id=oZ1RWDX3sQ3_VeS33GhgPWaNpf09xnVrnGGTXaLma64";
+
+//check if all images are loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+
+  }
+}
 
 // helper function
 function setAttributes(element, attributes) {
@@ -17,6 +30,8 @@ function setAttributes(element, attributes) {
 
 //create elements for links
 function displayPhotos() {
+  totalImages = photosArray.length;
+  imagesLoaded = 0;
   //foreach method
   photosArray.forEach((photo) => {
     const item = document.createElement('a');
@@ -30,6 +45,7 @@ function displayPhotos() {
       alt: photo.alt_description,
       title: photo.alt_description
     })
+    img.addEventListener('load', imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   })
@@ -50,7 +66,8 @@ async function getPhotos() {
 //check to see if we are near the bottom
 
 window.addEventListener('scroll', () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
     getPhotos();
   }
 })
